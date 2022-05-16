62864a03914118cc5fd6e67dc33acb2a10937119
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-16 16:41:34 +0530

    request accept or reject created

--- backend/controllers/supervisorController.js
+++ backend/controllers/supervisorController.js
@@ -33,6 +33,22 @@ const getSupervisorRequest  = asyncHandler(async (req, res) => {
 
 })
 
+//accept or reject request
+const requestCheck = asyncHandler(async (req, res) => {
+
+    const reqId = req.params.id;
+    const reqStates = req.body.requestStates;
+
+    try{
+        const updatedResult = await requestSupervisor.findByIdAndUpdate(reqId, {requestStates:reqStates} )
+        res.status(200).json(updatedResult);
+    }catch (err){
+        console.log(err)
+    }
+
+
+})
+
 // module.exports = {
 //     getAllRequestedSupervisors,
 //     getSupervisorRequest
@@ -40,6 +56,7 @@ const getSupervisorRequest  = asyncHandler(async (req, res) => {
 
 module.exports.getAllRequestedSupervisors = getAllRequestedSupervisors
 module.exports.getSupervisorRequest = getSupervisorRequest
+module.exports.requestCheck = requestCheck
 
 
 
