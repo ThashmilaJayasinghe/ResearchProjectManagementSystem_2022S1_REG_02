4b2433ac058f61358911ce3e13e6e2c49e3ebd4b
it20114312 <it20114312@my.sliit.lk>
2022-05-24 16:48:46 +0530

    student Controller, Model, Router

--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
-        // name:{
-        //     type:String,
-        //     required:true
-        // },
+
         user:{
             type: mongoose.Schema.Types.ObjectId,
             ref:'User'
@@ -15,21 +12,6 @@ const studentSchema = new Schema({
             type:String,
             required:true
         },
-        // email:{
-        //     type: String,
-        //     required: [true,'Please enter an email'],
-        //     unique: true,
-        //     lowercase: true,
-        // },
-        // password:{
-        //     type:String,
-        //     required: [true, 'Please enter a valid password'],
-        //     // minlength: [6, 'Minimum password length must be 6 characters']
-        // },
-        // registeredDate:{
-        //     type:Date,
-        //
-        // }
     },
     {
         timestamps: true
