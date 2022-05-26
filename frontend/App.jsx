19519e051751f69b22c2525c09de0185bf5d3cd0
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-26 23:04:32 +0530

    student chat added

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -8,6 +8,8 @@ import Request_ResearchField from "./pages/supervisor/Request_ResearchField";
 import RequestRF from "./pages/student/RequestRF";
 import ViewRequests from "./pages/student/ViewRequests";
 import Sup_profile from './pages/supervisor/Sup_profile';
+import Sup_chat from './pages/supervisor/Sup_chat';
+import Stu_chat from './pages/student/Stu_chat';
 
 const studentpages = [
     'Students',
@@ -50,7 +52,8 @@ export default class App extends React.Component {
                         <Route exact path = '/student/addRequest' element = {<RequestRF/>} />
                         <Route exact path= '/student/requests' element = {<ViewRequests/>} />
                         <Route exact path='/supervisor/profile' element = {<Sup_profile/>} />
-
+                        <Route exact path = '/supervisor/chat' element = {<Sup_chat/>}/>
+                        <Route exact path='/student/chat' element = {<Stu_chat/>} />
                     </Routes>
                 </div>
 
---
+++ frontend/pages/student/Stu_chat.jsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from 'react'
+import MDSpinner from "react-md-spinner";
+const appID = process.env.REACT_APP_ID;
+const region = process.env.REACT_APP_REGION;
+const AUTH_KEY = process.env.REACT_APP_AUTH_KEY;
+const wid = process.env.REACT_APP_W1;
+
+const Stu_chat = () => {
+
+    localStorage.setItem("cc-uid", "IT20123456")
+    localStorage.setItem("agent-uid", "nuwankodagoda")
+
+  console.log("user : " + localStorage.getItem("cc-uid"))
+  console.log("agent : " + localStorage.getItem("agent-uid"))
+
+  const [load, setLoad] = useState(true);
+    const [studentName, setStudentName] = useState("IT20253462Name");
+    const [studentEmail, setStudentEmail] = useState("")
+
+  useEffect(() => {
+    setLoad(true);
+    window.CometChatWidget.init({
+      appID: appID,
+      appRegion: region,
+      authKey: AUTH_KEY,
+    }).then((response) => {
+      console.log("Initialization completed successfully");
+      //You can now call login function.
+      let uid = localStorage.getItem("cc-uid");
+      if (uid === null || uid == "") {
+        // create new user
+        const uid = "user" + new Date().getSeconds().toString();
+        const user = new window.CometChatWidget.CometChat.User(uid);
+        user.setName(studentName);
+        window.CometChatWidget.createOrUpdateUser(user).then((user) => {
+          // Proceed with user login
+
+          window.CometChatWidget.login({
+            uid: uid,
+          }).then((loggedInUser) => {
+            localStorage.setItem("cc-uid", loggedInUser.uid);
+            // Proceed with launching your Chat Widget
+            window.CometChatWidget.launch({
+              widgetID: wid,
+              roundedCorners: "true",
+              docked: "true",
+              height: "500px",
+              width: "400px",
+              defaultID: localStorage.getItem("agent-uid"),
+              defaultType: "user", //user or group
+            });
+            setLoad(false);
+            console.log(loggedInUser)
+          }).catch(err => {
+            console.log("error occured when login : " + err)
+          });
+        });
+      } else {
+        window.CometChatWidget.login({
+          uid: uid,
+        }).then((user) => {
+          window.CometChatWidget.launch({
+            widgetID: wid,
+            roundedCorners: "true",
+            docked: "true",
+            height: "500px",
+            width: "400px",
+            defaultID: localStorage.getItem("agent-uid"),
+            defaultType: "user", //user or group
+          });
+          setLoad(false);
+          console.log(user)
+        }, 
+        (error) => {
+        
+          console.log("error in register: " + error )
+          const uid = localStorage.getItem("cc-uid")
+          const user = new window.CometChatWidget.CometChat.User(uid);
+          user.setName(studentName);
+          window.CometChatWidget.createOrUpdateUser(user).then((user) => {
+            window.CometChatWidget.login({
+              uid: uid,
+            }).then((loggedInUser) => {
+              localStorage.setItem("cc-uid", loggedInUser.uid);
+              // Proceed with launching your Chat Widget
+              window.CometChatWidget.launch({
+                widgetID: wid,
+                roundedCorners: "true",
+                docked: "true",
+                height: "500px",
+                width: "400px",
+                defaultID: localStorage.getItem("agent-uid"),
+                defaultType: "user", //user or group
+              });
+              setLoad(false);
+              console.log(loggedInUser)
+            }).catch(err => {
+              console.log("error occured when login : " + err)
+            });
+          });
+
+        }
+        )
+      }
+    });
+  }, []);
+  if (load) {
+    return (
+      <div className="container">
+        <MDSpinner />
+      </div>
+    );
+  }
+
+  return <div className="App"></div>
+
+}
+
+export default Stu_chat
