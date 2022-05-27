84c34aef3a15ac80978be4bdd08a9d2a30f5e5ef
it20114312 <it20114312@my.sliit.lk>
2022-05-27 19:49:45 +0530

    group frontend backend

--- backend/models/groupModel.js
+++ backend/models/groupModel.js
@@ -1,52 +1,47 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
-    // title:{
-    //     type:String
-    // },
-    subMemberRegNumber:{
-        type:String,
-    },
+	subMemberRegNumber: {
+		type: String,
+	},
 
-    members:[{
-        regNumber:{
-            type:String,
-        },
-        leader:{
-            type:Boolean,
-            default:false
-        },
-        // email:{
-        //     type: String,
-        //     // required: [true,'Please enter an email'],
-        //     unique: true,
-        //     lowercase: true,
-        // }
-    }],
-    reg_Date:{
-        type:Date,
-        default:Date.now()
-    },
-    supervisor:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Supervisor'
-    },
-    // supervisorName:{
-    //     type:String
-    // },
-    // coSupervisorName:{
-    //     type:String
-    // },
-    coSupervisor:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Supervisor'
-    }
+	members: [
+		{
+			regNumber: {
+				type: String,
+			},
+			leader: {
+				type: Boolean,
+				default: false,
+			},
+			email: {
+				type: String,
+				// required: [true,'Please enter an email'],
+				// unique: true,
+				// lowercase: true,
+			},
+		},
+	],
+	reg_Date: {
+		type: Date,
+		default: Date.now(),
+	},
+	supervisor: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Supervisor',
+	},
+	// supervisorName:{
+	//     type:String
+	// },
+	// coSupervisorName:{
+	//     type:String
+	// },
+	coSupervisor: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Supervisor',
+	},
+});
 
-})
-
-// function arrayLimit(val){
-//     return val.length <=4;
-// }
-const Group = mongoose.model('Group',groupSchema);
+const Group = mongoose.model('Group', groupSchema);
 module.exports = Group;
