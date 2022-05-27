9bf0783223c77094a40a58924d66513567ab2518
it20114312 <it20114312@my.sliit.lk>
2022-05-27 19:52:44 +0530

    topic status frontend backend

--- backend/models/topicStatusModel.js
+++ backend/models/topicStatusModel.js
@@ -2,37 +2,37 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const topicStatusSchema = new Schema({
-    grp_ID : {
-        type : String,
-        // required : true
-    },
-    title : {
-        type : String,
-        required : true
-    },
-    message:{
-        type:String
-    },
-    supervisorID : {
-        type : String,
-        // required : true
-    },
-    coSupervisorID : {
-        type : String,
-        // required : true
-    },
-    status : {
-        type : String,
-        // required : true
-    },
-    feedback : {
-        type : String
-    },
-    evaluated_Date : {
-        type : Date,
-        default: Date.now
-    },
-})
+	grp_ID: {
+		type: String,
+		// required : true
+	},
+	title: {
+		type: String,
+		required: true,
+	},
+	message: {
+		type: String,
+	},
+	supervisorID: {
+		type: String,
+		// required : true
+	},
+	coSupervisorID: {
+		type: String,
+		// required : true
+	},
+	status: {
+		type: String,
+		// required : true
+	},
+	feedback: {
+		type: String,
+	},
+	evaluated_Date: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
-const topicStatus = mongoose.model("topicStatus", topicStatusSchema);
+const topicStatus = mongoose.model('topicStatus', topicStatusSchema);
 module.exports = topicStatus;
