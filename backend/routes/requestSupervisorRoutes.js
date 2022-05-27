670909ba234bb292e5cea1b81c44a356a89c6eb4
it20114312 <it20114312@my.sliit.lk>
2022-05-27 19:51:07 +0530

    topic request frontend backend

--- backend/routes/requestSupervisorRoutes.js
+++ backend/routes/requestSupervisorRoutes.js
@@ -1,24 +1,30 @@
 const express = require('express');
-const {protect} = require("../middleware/authMiddleware");
-const router = express.Router()
-const makeSupervisorRequest = require('../controllers/requestSupervisorController').post_request
+const { protect } = require('../middleware/authMiddleware');
+const router = express.Router();
+const makeSupervisorRequest =
+	require('../controllers/requestSupervisorController').post_request;
+const makeCOSupervisorRequest =
+	require('../controllers/requestSupervisorController').post_Co_request;
 // const Requests = require('../controllers/requestSupervisorController').get_requests
 // const updateRequest = require('../controllers/requestSupervisorController').update_request
-const getAllRequestedSupervisors = require('../controllers/requestSupervisorController').getAllRequestedSupervisors
-const getSupervisorRequest = require('../controllers/requestSupervisorController').getSupervisorRequest
-const requestCheck = require('../controllers/requestSupervisorController').requestCheck
-
+const getAllRequestedSupervisors =
+	require('../controllers/requestSupervisorController').getAllRequestedSupervisors;
+const getSupervisorRequest =
+	require('../controllers/requestSupervisorController').getSupervisorRequest;
+const requestCheck =
+	require('../controllers/requestSupervisorController').requestCheck;
 
 //add a request
-router.post('/', makeSupervisorRequest)
+router.post('/supervisor', makeSupervisorRequest);
+router.post('/coSupervisor', makeCOSupervisorRequest);
 
 //get all the requests
-router.get('/requests',getAllRequestedSupervisors)
+router.get('/requests', getAllRequestedSupervisors);
 //
 // //get requests according to the supervisor
-router.get('/requestedSupervisor', getSupervisorRequest)
+router.get('/requestedSupervisor', getSupervisorRequest);
 //
 // //update request states
-router.put('/updateRequest/:id', requestCheck)
+router.put('/updateRequest/:id', requestCheck);
 
 module.exports = router;
