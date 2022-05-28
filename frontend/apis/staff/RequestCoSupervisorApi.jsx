4b66630ba714bec960379d999c647015836cf28c
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-28 14:55:51 +0530

    co-supervisor api added

---
+++ frontend/apis/staff/RequestCoSupervisorApi.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+//get all co-supervisor requests
+export const getAllCoSupervisorRequests = async (setRequestDetails) => {
+
+    try{
+        const response = await axios.get("http://localhost:5000/api/")
+            .then(res => setRequestDetails(res.data.response))
+
+        return response;
+    }catch (err){
+        console.log("Error occurred in getAllRequest")
+        console.error('Error')
+    }
+}
+
+//get data according to the co-supervisor
+export const getCoSupervisorRequests = async (staffEmail,setSupRequests) => {
+    try {
+        const response = await axios.get("http://localhost:5000/api/",{params:{supervisorEmail:staffEmail}})
+            .then((res) => setSupRequests([res.data]))
+    }catch (err){
+        console.log(err)
+    }
+}
+
+//change the states of the request
+export const changeCoSupervisorRequestStates = async (id, state) => {
+
+    try{
+        const response = await axios.put(`http://localhost:5000/api/reqSupervisor//${id}`, {requestStates: state})
+            .then((res) => console.log(res.data))
+    }catch (err) {
+        console.log(err)
+    }
+}
