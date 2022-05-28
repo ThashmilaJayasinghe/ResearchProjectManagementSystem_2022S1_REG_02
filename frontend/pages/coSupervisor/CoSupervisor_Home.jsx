18e162c735dc25b01d1af3a69dccf6d2ca1f41df
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-28 14:45:54 +0530

    co supervisor home added

---
+++ frontend/pages/coSupervisor/CoSupervisor_Home.jsx
@@ -0,0 +1,42 @@
+import { Box, Button, ButtonGroup } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const CoSupervisor_Home = () => {
+  return (        
+        <div style={{paddingTop: "2rem"}}>
+            co supervisor
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    '& > *': {
+                    m: 1,
+                    },
+                }}
+            >
+                <ButtonGroup variant="text" aria-label="text button group">
+
+                    <Link  to = '/co-supervisor/requestedresearchField'>
+                        <Button>View Requests</Button>
+                    </Link>
+                    <Link to = '#' style={{}}>
+                        <Button>Document evalueation</Button>
+                    </Link>
+
+{/* chat route and component that will called are same */}
+                    <Link to = '/' style={{}}>
+                        <Button>Chat</Button>
+                    </Link>
+                    
+                </ButtonGroup>
+
+            </Box>
+        </div>
+
+
+  )
+}
+
+export default CoSupervisor_Home
