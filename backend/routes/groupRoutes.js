aa25f891b24dc12e226e9a9711a269cbe3474f6b
it20114312 <it20114312@my.sliit.lk>
2022-05-28 13:33:51 +0530

    Group Updates

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const set_group = require('../controllers/groupController').set_group;
 const create_group = require('../controllers/groupController').create_group;
-// const get_id = require('../controllers/groupController').get_id;
+const get_Group = require('../controllers/groupController').get_Group;
 
 const router = express.Router();
 
 router.post('/create', create_group);
 router.post('/', set_group);
-// router.get('/', get_id);
+router.get('/:id', get_Group);
 
 module.exports = router;
