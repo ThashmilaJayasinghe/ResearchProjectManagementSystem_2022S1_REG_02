351554a13c71d173476d74d12e6833eaa97c4f8c
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-30 15:22:57 +0530

    updates student routes

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -4,14 +4,16 @@ const create_group = require('../controllers/groupController').create_group;
 const get_Group = require('../controllers/groupController').get_Group;
 const update_group_supervisor = require('../controllers/groupController').update_group_supervisor;
 const update_group_coSupervisor = require('../controllers/groupController').update_group_coSupervisor;
+const getGroupDetails = require('../controllers/groupController').getGroupDetails
 
 const router = express.Router();
 
 router.post('/create', create_group);
 router.post('/', set_group);
-router.get('/:id', get_Group);
+router.get('/getGroup/:id', get_Group);
 
 router.put('/updateSupervisor', update_group_supervisor)
 router.put('/updateCoSupervisor', update_group_coSupervisor)
+router.get('/getGroupDetails', getGroupDetails)
 
 module.exports = router;
