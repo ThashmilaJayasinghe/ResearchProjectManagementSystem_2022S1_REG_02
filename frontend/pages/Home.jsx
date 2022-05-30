7a44b2b02d79bd76d0638d3f4d79aabf50da3ab7
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-30 15:23:54 +0530

    home changed to test

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { getStudentId } from './student/studentAPI'
 
 const Home = () => {
 
+    const {user} = useSelector((state) => state.auth) //used to get the user
+    const [studentDetails, setStudentDetails] = useState("")
+
+    useEffect(() => {
+        // get student id
+        console.log("Called")
+        // async function getStudent() {
+        //     await getStudentId(user._id, setStudentDetails)
+        //         .then(() => console.log("Student got"))
+        // }
+        // getStudent()
+    }, [])
+
+    console.log("called here")
 
     return (
         <div>
             <div>
                 <h1>WELCOME TO RESEARCH PROJECT MANAGEMENTS </h1>
             </div>
-
-            <Link to = "/supervisor/requestedresearchField">
-                click to navigate
-            </Link>
-
         </div>
     )
 }
