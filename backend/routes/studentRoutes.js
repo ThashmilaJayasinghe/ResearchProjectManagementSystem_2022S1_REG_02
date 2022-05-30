6e59f895fcd5174c0d640c746facfc429f31f6ff
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-30 15:23:11 +0530

    updates student routes

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const isAStudent = require('../controllers/studentController').isAStudent
 const set_data = require('../controllers/studentController').set_data
 
 const router = express.Router();
 
 router.post('/:id',set_data);
+router.get('/isAvailabale/:id', isAStudent);
 
 module.exports = router;
 
