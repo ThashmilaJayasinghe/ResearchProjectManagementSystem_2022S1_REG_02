6bb1b3724d3f13ece462f1d332bb622f76b7b51d
it20114312 <it20114312@my.sliit.lk>
2022-05-30 10:14:02 +0530

    Admin Controller and router updates

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,83 +1,76 @@
-const Role = require('../models/roleModel')
-const User = require('../models/userModel')
-const SubmissionType = require('../models/submissionTypeModel')
-
+const Role = require('../models/roleModel');
+const User = require('../models/userModel');
+const SubmissionType = require('../models/submissionTypeModel');
 
 // @desc    Add role
 // @route   POST /api/admin/addRole
 // @access  Private
 const addRole = async (req, res) => {
-    if(!req.body.name) {
-        return res.status(400).json({ msg: 'Please add role'})
-    }
+	if (!req.body.name) {
+		return res.status(400).json({ msg: 'Please add role' });
+	}
 
-    const role = await Role.create({
-        name: req.body.name,
-    })
+	const role = await Role.create({
+		name: req.body.name,
+	});
 
-    res.status(200).json(role)
-}
-
+	res.status(200).json(role);
+};
 
 // @desc    Allocate role
 // @route   PUT /api/admin/allocateRole/:staffid
 // @access  Private
 const allocateRole = async (req, res) => {
-    const user = await User.findById(req.params.staffid)
+	const user = await User.findById(req.params.staffid);
 
-    // check for user
-    if(!user) {
-        return res.status(401).json({ msg: 'User not found'})
-    }
+	// check for user
+	if (!user) {
+		return res.status(401).json({ msg: 'User not found' });
+	}
 
-    user.roles.push(req.body.roles);
-    user.save();
+	user.roles.push(req.body.roles);
+	user.save();
 
-    const allocatedUser = await User.findByIdAndUpdate(req.params.staffid, user)
+	const allocatedUser = await User.findByIdAndUpdate(req.params.staffid, user);
 
-    res.status(200).json(allocatedUser)
-}
-
+	res.status(200).json(allocatedUser);
+};
 
 // @desc    Get all staff data
 // @route   GET /api/admin/staff
 // @access  Private
 const getStaff = async (req, res) => {
+	const staff = await User.find({ roles: 'staff' });
 
-    const staff = await User.find({roles:'staff'})
-
-    if(staff) {
-        res.status(200).json(staff)
-    } else {
-        return res.status(404).json({ msg: 'No staff to display'})
-    }
-}
-
+	if (staff) {
+		res.status(200).json(staff);
+	} else {
+		return res.status(404).json({ msg: 'No staff to display' });
+	}
+};
 
 // @desc    Add assignment
 // @route   POST /api/admin/addAssignment
 // @access  Private
 const addAssignment = async (req, res) => {
+	const { title, type, instructions } = req.body;
 
-    const{title, type, instructions, } = req.body
+	if (!name || !email || !password || !role) {
+		return res.status(400).json({ msg: 'Please add all fields' });
+	}
 
-    if(!name || !email || !password || !role) {
-        return res.status(400).json({ msg: 'Please add all fields'})
-    }
+	const role = await Role.create({
+		name: req.body.name,
+	});
 
-    const role = await Role.create({
-        name: req.body.name,
-    })
-
-    res.status(200).json(role)
-}
-
+	res.status(200).json(role);
+};
 
 
 
 module.exports = {
-    addRole,
-    allocateRole,
-    getStaff,
-    addAssignment,
-}
+	addRole,
+	allocateRole,
+	getStaff,
+	addAssignment,
+};
