a58631144247cbb167cd146d865ff39380164cee
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-30 15:22:10 +0530

    get student details api

--- frontend/pages/student/studentAPI.jsx
+++ frontend/pages/student/studentAPI.jsx
@@ -6,3 +6,13 @@ export const addSupervisorRequest = () => {
 
     console.log("data added", )
 }
+
+export const getStudentId = async(userId, setStudentDetails) => {
+    try{
+        const student = await axios.get(`http://localhost:5000/student/isAvailabale/${userId}`)
+            .then((res) => {setStudentDetails(res.data)})
+    }catch(err) {
+        console.log(err)
+        console.log("Error occured in getting student details")
+    }
+}
