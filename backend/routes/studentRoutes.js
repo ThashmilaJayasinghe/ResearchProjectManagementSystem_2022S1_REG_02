cfadfb07e667f149d962c965bd02dcb985436e05
it20114312 <it20114312@my.sliit.lk>
2022-05-31 19:13:18 +0530

    Student controller and routes

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const set_data = require('../controllers/studentController').set_data
-const getAllFiles = require('../controllers/studentController').getAllFiles
+const set_data = require('../controllers/studentController').set_data;
+const getAllFiles = require('../controllers/studentController').getAllFiles;
+const isAStudent = require('../controllers/studentController').isAStudent;
 
 const router = express.Router();
 
-router.post('/:id',set_data);
+router.post('/:id', set_data);
 router.get('/allSubmitTypes', getAllFiles);
+router.get('/isAvailabale/:id', isAStudent);
 
 module.exports = router;
-
