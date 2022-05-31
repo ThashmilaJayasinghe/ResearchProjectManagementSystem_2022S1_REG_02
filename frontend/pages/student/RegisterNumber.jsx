32785e9a0f60caae7e08c452e4e24fdfc614c8a7
it20114312 <it20114312@my.sliit.lk>
2022-05-31 19:14:58 +0530

    Frontend

---
+++ frontend/pages/student/RegisterNumber.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import TextField from "@mui/material/TextField";
+import React, { useEffect, useState } from "react";
+import {Button} from "@mui/material";
+import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
+
+export default function RegisterNumber() {
+
+    const { user } = useSelector((state) => state.auth);
+
+    const [regNumber,setRegNumber] = useState("");
+
+    const handleSubmit = ()=>{
+        axios.post('http://localhost:5000/student/'+user._id, {regNumber})
+        .then(()=>{
+            alert('Register Number Added');
+            setRegNumber("")
+        })
+    }
+    return(
+        <div style={{paddingTop:"20px"}}>
+            <div style={{width: "60%", margin: "auto", }}>
+                
+            <center><h1>Add Your Register Number</h1></center>
+                    <h4>Register Number</h4>
+                    <TextField fullWidth type="text" id="register" label="Register Number....." onChange={(e)=>(setRegNumber(e.target.value))}/>
+                
+                    <div style={{paddingTop: "20px"}}>
+                        <Link to='/'>
+                        <Button type="submit"  variant="contained" color="info" style={{marginRight: "5px"}} onClick={handleSubmit}>Register</Button>
+                        </Link>
+                </div>
+                </div> 
+        </div>
+    )
+}
