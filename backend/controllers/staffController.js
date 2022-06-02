c5d7da8d8c23c8a9a819a28fb5cc7f861a9ec74e
it20114312 <it20114312@my.sliit.lk>
2022-06-02 20:56:41 +0530

    Staff controller and routes changes

--- backend/controllers/staffController.js
+++ backend/controllers/staffController.js
@@ -21,103 +21,103 @@ module.exports.addStaff = asyncHandler(async (req, res) => {
 	}
 });
 
-
-module.exports.getAllSupervisors = asyncHandler(async (req, res) => {
+module.exports.getAllStaff = asyncHandler(async (req, res) => {
 	Staff.find()
-		.then((supervisor) => {
-			res.json(supervisor);
+		.then((Staff) => {
+			res.json(Staff);
 		})
 		.catch((err) => {
 			console.log(err);
 		});
 });
 
-//update the qualifications of supervisor 
-const addQualifications = asyncHandler(async(req, res) => {
+//update the qualifications of supervisor
+const addQualifications = asyncHandler(async (req, res) => {
+	const newQualification = req.body.qualifications;
+	const email = req.body.email;
 
-    const newQualification = req.body.qualifications;
-    const email = req.body.email;
+	try {
+		const updateResult = await Staff.findOneAndUpdate(
+			{ email: email },
+			{ $push: { qualifications: newQualification } }
+		);
+		res.status(200).json(updateResult);
+	} catch (err) {
+		console.log(err);
+	}
+});
 
-    try{
-        const updateResult = await Staff.findOneAndUpdate({email:email},{ $push : {qualifications: newQualification}})
-        res.status(200).json(updateResult)
-    }catch(err){
-        console.log(err)
-    }
-}) 
+//update the research field of supervisor
+const addResearchField = asyncHandler(async (req, res) => {
+	const newResearchFeild = req.body.researchInterests;
+	const email = req.body.email;
 
-//update the research field of supervisor 
-const addResearchField = asyncHandler(async(req, res) => {
-
-    const newResearchFeild = req.body.researchInterests;
-    const email = req.body.email;
-
-    try{
-        const updateResult = await Staff.findOneAndUpdate({email:email},{ $push : {researchInterests: newResearchFeild}})
-        res.status(200).json(updateResult)
-    }catch(err){
-        console.log(err)
-    }
-}) 
+	try {
+		const updateResult = await Staff.findOneAndUpdate(
+			{ email: email },
+			{ $push: { researchInterests: newResearchFeild } }
+		);
+		res.status(200).json(updateResult);
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 // get staff details
-const getStaffDetails = asyncHandler(async(req, res) => {
+const getStaffDetails = asyncHandler(async (req, res) => {
+	// const email = req.body.email;
+	const email = req.query.email;
 
-    // const email = req.body.email;
-    const email = req.query.email;
-
-    try{
-        const result = await Staff.find({email})
-        res.status(200).json(result)
-    }catch(err){
-        console.log(err)
-        res.json(err)
-    }
-})
+	try {
+		const result = await Staff.find({ email });
+		res.status(200).json(result);
+	} catch (err) {
+		console.log(err);
+		res.json(err);
+	}
+});
 
 // remove qualifications
-const removeQualifications = asyncHandler(async(req, res) => {
-    
-    const email = req.body.email;
-    // const indexValue = req.body.IndexVal;
-    const arrVal = req.body.value;
+const removeQualifications = asyncHandler(async (req, res) => {
+	const email = req.body.email;
+	// const indexValue = req.body.IndexVal;
+	const arrVal = req.body.value;
 
-    try{
-        const deleteVal = await Staff.findOneAndUpdate({email: email}, 
-            {
-                $pull: { qualifications: arrVal }
-            }
-        )
-        res.status(200).json(deleteVal)
-    }
-    catch(err){
-        console.log(err)
-        res.json(err)
-    }
-})
+	try {
+		const deleteVal = await Staff.findOneAndUpdate(
+			{ email: email },
+			{
+				$pull: { qualifications: arrVal },
+			}
+		);
+		res.status(200).json(deleteVal);
+	} catch (err) {
+		console.log(err);
+		res.json(err);
+	}
+});
 
-const removeResearchInterests = asyncHandler(async(req, res) => {
-    
-    const email = req.body.email;
-    // const indexValue = req.body.IndexVal;
-    const arrVal = req.body.value;
+const removeResearchInterests = asyncHandler(async (req, res) => {
+	const email = req.body.email;
+	// const indexValue = req.body.IndexVal;
+	const arrVal = req.body.value;
 
-    try{
-        const deleteVal = await Staff.findOneAndUpdate({email: email}, 
-            {
-                $pull: { researchInterests: arrVal }
-            }
-        )
-        res.status(200).json(deleteVal)
-    }
-    catch(err){
-        console.log(err)
-        res.json(err)
-    }
-})
+	try {
+		const deleteVal = await Staff.findOneAndUpdate(
+			{ email: email },
+			{
+				$pull: { researchInterests: arrVal },
+			}
+		);
+		res.status(200).json(deleteVal);
+	} catch (err) {
+		console.log(err);
+		res.json(err);
+	}
+});
 
-module.exports.addQualifications = addQualifications
-module.exports.addResearchField = addResearchField
-module.exports.getStaffDetails = getStaffDetails
-module.exports.removeQualifications = removeQualifications
-module.exports.removeResearchInterests = removeResearchInterests
+module.exports.addQualifications = addQualifications;
+module.exports.addResearchField = addResearchField;
+module.exports.getStaffDetails = getStaffDetails;
+module.exports.removeQualifications = removeQualifications;
+module.exports.removeResearchInterests = removeResearchInterests;
--- backend/routes/staffRoutes.js
+++ backend/routes/staffRoutes.js
@@ -1,31 +1,34 @@
 const express = require('express');
-const { addQualifications, addResearchField, getStaffDetails, removeQualifications, removeResearchInterests } = require('../controllers/staffController');
+const {
+	addQualifications,
+	addResearchField,
+	getStaffDetails,
+	removeQualifications,
+	removeResearchInterests,
+} = require('../controllers/staffController');
 
-const getAllSupervisors =
-	require('../controllers/staffController').getAllSupervisors;
+const getAllStaff = require('../controllers/staffController').getAllStaff;
 
 const addStaff = require('../controllers/staffController').addStaff;
-
 
 const router = express.Router();
 
 router.post('/', addStaff);
 
-router.get('/', getAllSupervisors);
-
+router.get('/', getAllStaff);
 
 // get staff details
-router.get('/getStaff', getStaffDetails)
+router.get('/getStaff', getStaffDetails);
 
 // update qualifications
-router.put('/updateQual',addQualifications )
+router.put('/updateQual', addQualifications);
 
 // update research interest
-router.put('/updateResearch', addResearchField)
+router.put('/updateResearch', addResearchField);
 
 // delete qualification
-router.put('/deleteQual', removeQualifications)
+router.put('/deleteQual', removeQualifications);
 // delete research interest
-router.put('/deleteResearch', removeResearchInterests)
+router.put('/deleteResearch', removeResearchInterests);
 
 module.exports = router;
