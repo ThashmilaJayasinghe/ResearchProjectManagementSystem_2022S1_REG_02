aceb9f44c535fcc158e3e92a60de1221fcd6f824
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-06-04 10:24:05 +0530

    agent name whitespaces removed

--- frontend/pages/student/Instructor.jsx
+++ frontend/pages/student/Instructor.jsx
@@ -92,8 +92,9 @@ export default function Instructor() {
 
     if(supervisor){
       const tempVal = supervisor.supervisorName;
-      console.log(tempVal)
-      localStorage.setItem("agent-uid", tempVal)
+      const tempName = tempVal.replace(/ /g, '')
+
+      localStorage.setItem("agent-uid", tempName)
       localStorage.setItem("supChat", true)
 
       window.location.reload()
@@ -103,8 +104,9 @@ export default function Instructor() {
   const onCoSupervisorChat = (coSupervisor) => {
     if(coSupervisor){
       const tempVal = coSupervisor.supervisorName;
-      console.log(tempVal)
-      localStorage.setItem("agent-uid", tempVal)
+      const tempName = tempVal.replace(/ /g, '')
+
+      localStorage.setItem("agent-uid", tempName)
       setIsCoSupervisorChat(true)
       localStorage.setItem("coSupChat", true)
 
