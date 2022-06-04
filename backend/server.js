d3e76039f4c565d79ba3da3b0a72880b0d2cde4e
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-06-04 10:50:19 +0530

    some changes after merge

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, 'submissions')));
 
 
-app.use('/api/goals', require('./routes/goalRoutes'));
+// app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/topic', require('./routes/topicStatuesRoutes'));
