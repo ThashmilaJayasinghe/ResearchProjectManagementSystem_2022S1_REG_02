b51e803f939b356b7eaa66dfad9006946c47c466
Thashmila Jayasinghe <thashmilajayasinghe@gmail.com>
2022-06-04 17:59:11 +0530

    Updates to viewPanels page - added buttons

--- frontend/pages/admin/ViewPanels.jsx
+++ frontend/pages/admin/ViewPanels.jsx
@@ -19,7 +19,8 @@ import {useSelector} from "react-redux";
 export default function SelectUser() {
 
     const [panels, setPanels] = useState([]);
-    const [member, setMember] = useState("");
+    const [staffNames, setStaffNames] = useState([]);
+    const [groupName, setGroupName] = useState("")
 
     const navigate = useNavigate()
 
@@ -40,14 +41,6 @@ export default function SelectUser() {
             .then((res) => {
                 setPanels(res.data)
             })
-            // }).then(() => {
-            //     panels.map((panel) => {
-            //         Axios.get("http://localhost:5000/api/users/" + panel, config)
-            //             .then((res) => {
-            //                 setMember(res.data.name)
-            //     })
-            // })
-
 
     }, [user, navigate]);
 
@@ -75,12 +68,60 @@ export default function SelectUser() {
                                 <TableCell>Name</TableCell>
                                 <TableCell>Staff Members</TableCell>
                                 <TableCell>Group</TableCell>
-                                {/*<TableCell>Actions</TableCell>*/}
+                                <TableCell>Actions</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
                             {panels
                                 .map((panel, index) => {
+
+                                    const getPanels = () => {
+                                        Axios.get("http://localhost:5000/api/admin/allPanels", config)
+                                            .then((getPanels) => {
+                                                setPanels(getPanels.data);
+                                            })
+                                            .catch((err) => {
+                                                alert(err)
+                                            })
+                                    }
+
+                                    const onDelete = (id) => {
+
+                                        if (window.confirm('Do you wish to delete this panel?')) {
+                                            Axios.delete("http://localhost:5000/api/admin/deletePanel/" + id, config)
+                                                .then(() => {
+                                                    getPanels();
+                                                    alert("Panel Deleted");
+                                                })
+                                        }
+                                    }
+
+                                    const viewGroup = (id) => {
+
+                                        Axios.get("http://localhost:5000/api/admin/group/" + id, config)
+                                            .then((res) => {
+                                                setGroupName(res.data)
+                                                console.log(res.data)
+                                                console.log(groupName)
+                                            })
+                                    }
+
+                                    const viewStaff = (staff) => {
+
+                                            staff.map((smember) => {
+
+                                                Axios.get("http://localhost:5000/api/users/" + smember, config)
+                                                    .then((res) => {
+                                                        setStaffNames(res.data)
+                                                        console.log(res.data)
+                                                        // console.log(staffNames)
+                                                    })
+                                            })
+
+                                        // console.log(staff)
+                                    }
+
+
                                     return (
                                         <TableRow
                                             key={index}
@@ -90,20 +131,37 @@ export default function SelectUser() {
                                                 {panel.name}
                                             </TableCell>
                                             <TableCell sx={{ width: "30%" }}>
-                                                {panel.staff.map((pmember) => {
+                                                {/*{panel.staff.map((pmem) => {*/}
+                                                {/*    return (*/}
+                                                {/*        <li>{pmem}</li>*/}
 
-                                                    return (
-                                                        <li>{pmember}</li>
-                                                    )
-                                            })}
+                                                {/*    )*/}
+                                                {/*})}*/}
+                                                <Button
+                                                    variant="contained"
+                                                    style={{maxHeight: "30px", fontSize: "12px", backgroundColor: "#053769", marginTop: "0.5rem" }}
+                                                    onClick={() => {viewStaff(panel.staff)}}
+                                                >
+                                                    View Staff Members
+                                                </Button>
                                             </TableCell>
                                             <TableCell sx={{ width: "30%" }}>
-                                                {panel.groups.map((group) => {
-                                                    return (
-                                                        <li>{group}</li>
-
-                                                    )
-                                                })}
+                                                <Button
+                                                    variant="contained"
+                                                    style={{maxHeight: "30px", fontSize: "12px", backgroundColor: "#053769", marginTop: "0.5rem" }}
+                                                    onClick={() => {viewGroup(panel.groups[0])}}
+                                                >
+                                                    View Group
+                                                </Button>
+                                            </TableCell>
+                                            <TableCell sx={{ width: "10%" }} align="center">
+                                                <div style={{ display: "flex", gap: "1rem" }}>
+                                                    <div style={{cursor: "pointer"}} onClick={() => onDelete(panel._id)}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width='1.2rem' height="1.2rem" fill="none" viewBox="0 0 24 24" stroke="#ed2121" strokeWidth={2}>
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                        </svg>
+                                                    </div>
+                                                </div>
                                             </TableCell>
                                         </TableRow>
                                     )
