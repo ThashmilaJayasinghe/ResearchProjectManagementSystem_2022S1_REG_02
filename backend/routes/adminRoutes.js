aec0cd7227c88a8cbcc09c60371eff3b19ad142f
Thashmila Jayasinghe <thashmilajayasinghe@gmail.com>
2022-06-04 10:41:07 +0530

    get group details added to admin controller in BE

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { addRole, allocateRole, getStaff, getStudents, addAssignment, upload, addPanel, getGroups, getAllPanels, deleteSubmissionType } = require('../controllers/adminController')
+const { addRole, allocateRole, getStaff, getStudents, addAssignment, upload, addPanel, getGroup, getAllPanels, deleteSubmissionType } = require('../controllers/adminController')
 const { protect, authRole } = require('../middleware/authMiddleware')
 // actual route is /api/admin/
 
@@ -10,7 +10,7 @@ router.get('/staff', protect, authRole('admin'), getStaff)
 router.get('/students', protect, authRole('admin'), getStudents)
 router.post('/addAssignment', protect, authRole('admin'), upload.fields([{name: 'markingScheme', maxCount: 1}, {name: 'template', maxCount: 1}]), addAssignment)
 router.post('/addPanel', protect, authRole('admin'), addPanel)
-router.get('/groups', protect, authRole('admin'), getGroups)
+router.get('/group', protect, authRole('admin'), getGroup)
 router.get('/allPanels', protect, authRole('admin'), getAllPanels)
 router.delete('/deleteAssignment/:id', protect, authRole('admin'), deleteSubmissionType)
 
