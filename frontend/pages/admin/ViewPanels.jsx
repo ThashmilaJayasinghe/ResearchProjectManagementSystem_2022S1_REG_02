18296fb232b5fcc3aba65f9a4ffe381f8172e362
Thashmila Jayasinghe <thashmilajayasinghe@gmail.com>
2022-06-04 19:19:29 +0530

    Changes to View Panels modals

--- frontend/pages/admin/ViewPanels.jsx
+++ frontend/pages/admin/ViewPanels.jsx
@@ -7,20 +7,43 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Typography from "@mui/material/Typography";
-import {TextField} from "@mui/material";
+import {List, ListItem, TextField} from "@mui/material";
 import {useState, useEffect, useRef} from "react";
 import Axios from "axios";
-import fileDownload from 'js-file-download'
 import Button from "@mui/material/Button";
 import {useNavigate} from "react-router-dom";
 import {useSelector} from "react-redux";
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #053769',
+    borderRadius: '8px',
+    boxShadow: 24,
+    p: 4,
+};
 
 
 export default function SelectUser() {
 
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const [open2, setOpen2] = React.useState(false);
+    const handleOpen2 = () => setOpen2(true);
+    const handleClose2 = () => setOpen2(false);
+
     const [panels, setPanels] = useState([]);
     const [staffNames, setStaffNames] = useState([]);
     const [groupName, setGroupName] = useState("")
+
 
     const navigate = useNavigate()
 
@@ -103,7 +126,9 @@ export default function SelectUser() {
                                                 setGroupName(res.data)
                                                 console.log(res.data)
                                                 console.log(groupName)
+                                                handleOpen()
                                             })
+
                                     }
 
                                     const viewStaff = (staff) => {
@@ -114,11 +139,9 @@ export default function SelectUser() {
                                                     .then((res) => {
                                                         setStaffNames(res.data)
                                                         console.log(res.data)
-                                                        // console.log(staffNames)
+                                                        handleOpen2()
                                                     })
                                             })
-
-                                        // console.log(staff)
                                     }
 
 
@@ -131,12 +154,6 @@ export default function SelectUser() {
                                                 {panel.name}
                                             </TableCell>
                                             <TableCell sx={{ width: "30%" }}>
-                                                {/*{panel.staff.map((pmem) => {*/}
-                                                {/*    return (*/}
-                                                {/*        <li>{pmem}</li>*/}
-
-                                                {/*    )*/}
-                                                {/*})}*/}
                                                 <Button
                                                     variant="contained"
                                                     style={{maxHeight: "30px", fontSize: "12px", backgroundColor: "#053769", marginTop: "0.5rem" }}
@@ -144,6 +161,36 @@ export default function SelectUser() {
                                                 >
                                                     View Staff Members
                                                 </Button>
+                                                <Modal
+                                                    open={open2}
+                                                    onClose={handleClose2}
+                                                    aria-labelledby="modal-modal-title"
+                                                    aria-describedby="modal-modal-description"
+                                                >
+                                                    <Box sx={style}>
+                                                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                                                            Staff Members:
+                                                        </Typography>
+                                                        <List>
+                                                            <ListItem disablePadding>
+                                                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                                                    Harry Potter
+                                                                </Typography>
+                                                            </ListItem>
+                                                            <ListItem disablePadding>
+                                                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                                                    Mary Smith
+                                                                </Typography>
+                                                            </ListItem>
+                                                            <ListItem disablePadding>
+                                                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                                                    Ann Murray
+                                                                </Typography>
+                                                            </ListItem>
+                                                        </List>
+                                                    </Box>
+                                                </Modal>
+
                                             </TableCell>
                                             <TableCell sx={{ width: "30%" }}>
                                                 <Button
@@ -153,6 +200,18 @@ export default function SelectUser() {
                                                 >
                                                     View Group
                                                 </Button>
+                                                <Modal
+                                                    open={open}
+                                                    onClose={handleClose}
+                                                    aria-labelledby="modal-modal-title"
+                                                    aria-describedby="modal-modal-description"
+                                                >
+                                                    <Box sx={style}>
+                                                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                                                            Group Name: {groupName}
+                                                        </Typography>
+                                                    </Box>
+                                                </Modal>
                                             </TableCell>
                                             <TableCell sx={{ width: "10%" }} align="center">
                                                 <div style={{ display: "flex", gap: "1rem" }}>
